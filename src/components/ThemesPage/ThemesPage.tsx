ffc4297405283515315efa8163010c72d4082196
Alexey <alex967268@gmail.com>
2022-01-09 18:21:44 +0300

    update navbar: added Themes, icons

---
+++ src/components/ThemesPage/ThemesPage.tsx
@@ -0,0 +1,15 @@
+import "./ThemesPage.css"
+
+const themes = [
+    { name: "default", isDarkTheme: false },
+    { name: "github", isDarkTheme: true },
+    { name: "fideloper", isDarkTheme: true },
+    { name: "atomone-light", isDarkTheme: false },
+    { name: "bright-lights", isDarkTheme: false },
+]
+
+const ThemesPage = () => {
+    return <div className='themes-page'>ThemesPage</div>
+}
+
+export { ThemesPage }
